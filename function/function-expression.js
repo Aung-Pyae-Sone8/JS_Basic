d94801adff1add7f472d12dd21fa461648fa17bf
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2023-12-18 18:07:03 +0630

    function-expression

---
+++ function/function-expression.js
@@ -0,0 +1,7 @@
+console.log("------------------------------------");
+console.log("-------function expression----------");
+
+let sayHi = function(word) {
+    console.log(word);
+}
+sayHi("hi there");
