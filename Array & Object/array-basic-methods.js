72217d160a188e53fe8516c773eff878d250ceb3
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2023-12-19 18:55:03 +0630

    array-basic-methods

---
+++ Array & Object/array-basic-methods.js
@@ -0,0 +1,13 @@
+// Array Methods
+
+let animals = ['cat', 'dog', 'lion'];
+
+// animals.push('pig');                    
+// animals.pop();
+// animals.shift();
+// animals.unshift('bird');
+// console.log(animals.indexOf('dog'));
+// console.log(animals.join('-'));
+animals.splice(1,1);
+
+console.log(animals);
