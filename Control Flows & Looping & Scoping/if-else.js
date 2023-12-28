b949e85b3689c5c66b4349e8aa47de08accde3ec
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2023-12-28 18:32:19 +0630

    if-else

---
+++ Control Flows & Looping & Scoping/if-else.js
@@ -0,0 +1,10 @@
+// conditions (if else) 
+
+let tired = prompt("Are you tired? 'yes/no/");
+
+if (tired === "yes")
+    console.log('rest well');
+else if(tired === 'no')
+    console.log('go back to work');
+else 
+    console.log('Please enter yes or no!');
