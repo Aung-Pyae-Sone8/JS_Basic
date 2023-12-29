c5ed6673f0431b9196b0e6b9bfa3aa0608f766f4
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2023-12-29 19:00:54 +0630

    while-loop

---
+++ Control Flows & Looping & Scoping/while-loop.js
@@ -0,0 +1,10 @@
+// while loop 
+
+let people2 = ['mg mg', 'aung aung', 'kyaw kyaw', 'su su'];
+
+let count = 0;
+
+while(count < people2.length) {
+    console.log("hello " + people2[count]);
+    count++;
+}
