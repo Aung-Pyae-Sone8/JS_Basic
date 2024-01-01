d84ca24226dd8d76b50f5742ebcd39285a185490
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-01 19:10:41 +0630

    static-method-and-this-keyword-in-classes

---
+++ Object Oriented Programming & Thinking (OOP)/static-method-and-this-keyword-in-classes.js
@@ -0,0 +1,26 @@
+// static method 
+
+class Calculator1{
+    static PI = 3.14;
+    static area(r){
+        return 3.14*r**2; 
+    }
+}
+
+console.log(Calculator1.area(2));
+
+// this keyword 
+
+class Calculator2{
+    PI = 3.14;
+    area(r){
+        return this.PI*r**2;
+    }
+    volume(r,h){
+        return this.area(r)*h;
+    }
+}
+
+let calculator2 = new Calculator2;
+console.log(calculator2.area(2));
+console.log(calculator2.volume(2,3));
