e4df092b322b8df880d58e22edef72d8637d662c
Aung-Pyae-Sone8 <apyae8263@gmail.com>
2024-01-01 19:40:07 +0630

    access-modifier

--- Object Oriented Programming & Thinking (OOP)/constructor-and-access-modifier.js
+++ Object Oriented Programming & Thinking (OOP)/constructor-and-access-modifier.js
@@ -19,3 +19,18 @@ new Car1('marcedes', 4);
 
 let toyota = new Car1('toyata', 10);
 toyota.getWheel();
+
+// OOP (access modifier) (static, public, private, protected(not in JS))
+
+class Car2{
+    Name = 'marcedes';  // (public)
+    #hp = 300;          // (# -> private)
+    getHorsePower(){
+        console.log('Horse Power is '+ this.#hp);
+    }
+}
+
+let car2 = new Car2;
+// car2.#hp = 600; (do not work beacause #hp is private)
+
+car2.getHorsePower();
